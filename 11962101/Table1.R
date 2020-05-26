35aecd21a30ea0f9f9fed83cfd921e014e8413ee
JASA Applications & Case Studies <JASA.app.cs.aer@gmail.com>
2020-05-26 16:13:49 -0400

    redundancy removal
    redundancy removal

--- 11962101/Table1.R
+++
@@ -1,8 +0,0 @@
-## wordk10.RData contains 10 word-sequences, corresponding to the scenario where the number of topics K=10.
-## Each word sequence contains the top 20 words in one topic after performing LDA with K=10, and over 50 repetitions, so of dimension 20*50.
-## The words we have are in Chinese, then we add English translation manually.
-
-### data files Table1.txt and wordk10.RData are by-products of SearchingK.R
-
-
-load("wordk10.RData")
